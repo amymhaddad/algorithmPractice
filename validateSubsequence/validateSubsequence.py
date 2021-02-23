664e24968ee83bad65d033de1bdf1b15bca5fafd
Amy M Haddad <amy.m.haddad@gmail.com>
2021-02-23 09:00:36 -0500

    Solved it

--- validateSubsequence/validateSubsequence.py
+++ validateSubsequence/validateSubsequence.py
@@ -1,37 +1,38 @@
-
 array = [5, 1, 22, 25, 6, -1, 8, 10]
 sequence = [1, 6, -1, 10]
-
+# # 
 # array = [5, 1, 22, 25, 6, -1, 8, 10]
- #sequence = [5, 1, 25, 22, 6, -1, 8, 10]
+# sequence = [5, 1, 25, 22, 6, -1, 8, 10]
 
-# array = [5, 1, 22, 25, 6, -1, 8, 10]
-# sequence = [5, 1, 22, 22, 25, 6, -1, 8, 10]
+#array = [1, 1, 1, 1, 1]
+#sequence = [1, 1, 1]
+
 
 def validateSubsequenceuence(array, sequence):
 
-    array_vals = {num: True for num in array }
 
-    for num in sequence:
-        if num not in array_vals:
-            return False
-        else:
-            del array_vals[num]
-    return True
-
-#print(validateSubsequenceuence(array, sequence))
-
-def validate(array, sequence):
-
-    valid_arr = []
     for num in array:
-        if num in sequence:
-            valid_arr.append(num)
-    return set(valid_arr) == set(sequence) 
+        if num not in sequence:
+            array.pop(num)
+    return array
 
-print(validate(array, sequence))
-
-
+print(validateSubsequenceuence(array, sequence))
 
 
+# def validate(array, sequence):
+
+#     seq_nums_indexes = {}
+#     seq_index = 0
+
+#     for num in array:
+#         if seq_index >= len(sequence):
+#             break
+#         if num in sequence:
+#             seq_nums_indexes[seq_index] = num
+#             seq_index += 1
+    
+#     matched_seq = {i: num   for i, num in enumerate(sequence)}
+#     return seq_nums_indexes == matched_seq
+
+# print(validate(array, sequence))
 
