f7a80562abf6ab34339131df0c8bfe4e24a6f681
Amy M Haddad <amy.m.haddad@gmail.com>
2021-02-23 10:00:14 -0500

    Final changes

--- validateSubsequence/validateSubsequence.py
+++ validateSubsequence/validateSubsequence.py
@@ -1,38 +1,18 @@
-array = [5, 1, 22, 25, 6, -1, 8, 10]
-sequence = [1, 6, -1, 10]
-# # 
-# array = [5, 1, 22, 25, 6, -1, 8, 10]
-# sequence = [5, 1, 25, 22, 6, -1, 8, 10]
-
-#array = [1, 1, 1, 1, 1]
-#sequence = [1, 1, 1]
+"""
+Given two non-empy arrays of integers, write a funciton taht dtermiens whether the second array is a subsequence of the first.
+"""
 
 
-def validateSubsequenceuence(array, sequence):
-
+def validate(array, sequence):
+    seq_nums_indexes = {}
+    seq_index = 0
 
     for num in array:
-        if num not in sequence:
-            array.pop(num)
-    return array
+        if seq_index >= len(sequence):
+            break
+        if num in sequence:
+            seq_nums_indexes[seq_index] = num
+            seq_index += 1
 
-print(validateSubsequenceuence(array, sequence))
-
-
-# def validate(array, sequence):
-
-#     seq_nums_indexes = {}
-#     seq_index = 0
-
-#     for num in array:
-#         if seq_index >= len(sequence):
-#             break
-#         if num in sequence:
-#             seq_nums_indexes[seq_index] = num
-#             seq_index += 1
-    
-#     matched_seq = {i: num   for i, num in enumerate(sequence)}
-#     return seq_nums_indexes == matched_seq
-
-# print(validate(array, sequence))
-
+    matched_seq = {i: num for i, num in enumerate(sequence)}
+    return seq_nums_indexes == matched_seq
