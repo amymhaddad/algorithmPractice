e2f2faf342e61aa5c34cf59ed8f6f03c37e405ed
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-05 07:38:58 -0500

    Solved problem

--- algoExpert/tournamentWinner/tournamentWinner.py
+++ algoExpert/tournamentWinner/tournamentWinner.py
@@ -2,27 +2,36 @@
 https://www.algoexpert.io/questions/Tournament%20Winner
 """
 
-competitions = [["HTML", "C#"], ["C#", "Python"], ["Python", "HTML"]]
-results = [0, 0, 1]
+competitions = [["HTML", "Java"], ["Java", "Python"], ["Python", "HTML"]]
+results = [0, 1, 1]
 
-def tournamentWinner(competitions, results):
-    
+
+def calculate_scores(competitions, results):
     total_scores = {}
-    i = 0
     win_points = 3
 
-    while i < len(results):
-        for team in competitions:
-            hometeam, awayteam = team[0], team[1]
-            if results[i] == 0:
-                total_scores[awayteam] = total_scores.get(awayteam, 0) + win_points
-                total_scores[hometeam] = total_scores.get(hometeam, 0) + 0 
+    for i, team in enumerate(competitions):
+        hometeam, awayteam = team[0], team[1]
+        if results[i] == 0:
+            total_scores[awayteam] = total_scores.get(awayteam, 0) + win_points
+            total_scores[hometeam] = total_scores.get(hometeam, 0) + 0
 
-            else:
-                total_scores[hometeam] = total_scores.get(hometeam, 0) + win_points
-                total_scores[awayteam] = total_scores.get(awayteam, 0) + 0 
+        else:
+            total_scores[hometeam] = total_scores.get(hometeam, 0) + win_points
+            total_scores[awayteam] = total_scores.get(awayteam, 0) + 0
+    return total_scores
 
-            i += 1
-        print(total_scores)
+
+def tournamentWinner(competitions, results):
+    total_scores = calculate_scores(competitions, results)
+    winner = ""
+    max_score_so_far = 0
+
+    for team, score in total_scores.items():
+        if score > max_score_so_far:
+            max_score_so_far = score
+            winner = team
+    return winner
+
 
 print(tournamentWinner(competitions, results))
