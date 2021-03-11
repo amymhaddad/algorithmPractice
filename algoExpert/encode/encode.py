56aa0061b6f4814d08c77b4cfe2a41b1a01868ed
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-11 07:22:00 -0500

    First algo works

--- algoExpert/encode/encode.py
+++ algoExpert/encode/encode.py
@@ -4,17 +4,27 @@ https://www.algoexpert.io/questions/Run-Length%20Encoding
 
 import re
 string = "A  **(())---         AA880001BB123"
-matched_letters = re.findall(r'[A-Z]*', string)
-#print(matched_letters)
+
+def get_letters(string):
+    return [letter  for letter in string if letter.isalpha()]
+
+def count_letters(letters):
+    letter_counts = {}
+    for letter in letters:
+        letter_counts[letter] = letter_counts.get(letter, 0) + 1
+    return letter_counts
 
 
-all_letters = []
-for letter in string:
-    if letter.isalpha():
-        all_letters.append(letter)
+def runLengthEncoding(letter_counts):
+    encode_string = []
 
+    for letter, count in letter_counts.items():
+        while count >= 10:
+            encode_string.extend([letter, str(9)])
+            count -= 9
+        encode_string.extend([letter, str(count)])
+    return "".join(encode_string)
 
-all_counts = {}
-for letter in all_letters:
-    all_counts[letter] = all_counts.get(letter, 0) + 1
-print(all_counts)
+letters = get_letters(string)
+counts = count_letters(letters)
+print(runLengthEncoding(counts))
