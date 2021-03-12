092e169dade010a023e9ef67cc77a674aa2912f4
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-12 10:19:38 -0500

    First commit

---
+++ leetCode/mergeLinkedLists/merge.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+
+        elif l1.val <= l2.val:
+            return ListNode(l1.val, mergeTwoLists(l2, l1.next))
+        else:
+            return ListNode(l2.val, mergeTwoLists(l1, l2.next))
+            
