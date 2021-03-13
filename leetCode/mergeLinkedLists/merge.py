63fb843223805fc2ba016993e12aca110966c121
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-13 07:17:47 -0500

    Solved problem

--- leetCode/mergeLinkedLists/merge.py
+++ leetCode/mergeLinkedLists/merge.py
@@ -1,7 +1,14 @@
+"""
+https://leetcode.com/problems/merge-two-sorted-lists/
+"""
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if l1 is None:
@@ -10,7 +17,6 @@ class Solution:
             return l1
 
         elif l1.val <= l2.val:
-            return ListNode(l1.val, mergeTwoLists(l2, l1.next))
+            return ListNode(l1.val, self.mergeTwoLists(l2, l1.next))
         else:
-            return ListNode(l2.val, mergeTwoLists(l1, l2.next))
-            
+            return ListNode(l2.val, self.mergeTwoLists(l1, l2.next))
