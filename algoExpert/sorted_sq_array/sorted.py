45710186f7e9e995b04e849cdd37692a0db8fad4
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-15 11:26:35 -0400

    Completed problem

--- algoExpert/sorted_sq_array/sorted.py
+++ algoExpert/sorted_sq_array/sorted.py
@@ -2,11 +2,13 @@
 https://www.algoexpert.io/questions/Sorted%20Squared%20Array
 """
 
+
 def sq_array(array):
     return [num * num for num in array]
 
+
 def smallest_index(array):
-    smallest_index = 0 
+    smallest_index = 0
     smallest_value = array[0]
 
     for i in range(1, len(array)):
@@ -17,13 +19,13 @@ def smallest_index(array):
 
     return smallest_index
 
+
 def sorted_squared_array(array):
     squared_array = sq_array(array)
     sorted_array = []
-  
+
     for i in range(len(squared_array)):
         next_smallest_index = smallest_index(squared_array)
         next_smallest_value = squared_array.pop(next_smallest_index)
         sorted_array.append(next_smallest_value)
-    return sorted_array 
-
+    return sorted_array
