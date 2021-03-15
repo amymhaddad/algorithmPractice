ed3e0bd8b6f5e00a247c422635fb16e9f855221d
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-15 07:31:57 -0400

    First iteration works:

--- algoExpert/sorted_sq_array/sorted.py
+++ algoExpert/sorted_sq_array/sorted.py
@@ -2,9 +2,6 @@
 https://www.algoexpert.io/questions/Sorted%20Squared%20Array
 """
 
-#array = [1, 2, 3, 5, 6, 8, 9]
-array = [-2, -1]
-
 def sq_array(array):
     return [num * num for num in array]
 
@@ -20,24 +17,13 @@ def smallest_index(array):
 
     return smallest_index
 
-#print(smallest_index(array))
-
 def sorted_squared_array(array):
     squared_array = sq_array(array)
     sorted_array = []
   
     for i in range(len(squared_array)):
-            
-        #import pdb; pdb.set_trace()
         next_smallest_index = smallest_index(squared_array)
-        next_smallest_value = squared_array[next_smallest_index]
-        if len(squared_array) == 1:
-            sorted_array.append(next_smallest_value)
-            break
-
-        squared_array.pop(next_smallest_value)
+        next_smallest_value = squared_array.pop(next_smallest_index)
         sorted_array.append(next_smallest_value)
-
     return sorted_array 
 
-print(sorted_squared_array(array))
