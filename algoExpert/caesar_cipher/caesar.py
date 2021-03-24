d95f976abcf1a14c137015c1c1c30049f53ce68a
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-24 09:50:49 -0400

    First iteration of algo - wip

--- algoExpert/caesar_cipher/caesar.py
+++ algoExpert/caesar_cipher/caesar.py
@@ -6,6 +6,15 @@ from string import ascii_lowercase as lower
 
 def caeasr(string, key):
     d = {i: letter for i, letter in enumerate(lower, 1)}
-    print(d)
+   
+    new_string = [] 
+    for index, letter in d.items():
+        if letter in string:
+
+            new_index = index + key
+            if new_index > 26:
+                new_index = new_index % 26
+            new_string.append(d[new_index])
+    return new_string
 
 print(caeasr("xyz", 2))
