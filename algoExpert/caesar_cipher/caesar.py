7e7947bebcb5ca54070960c1e5f28898965e20e5
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-25 10:15:39 -0400

    solved problem

--- algoExpert/caesar_cipher/caesar.py
+++ algoExpert/caesar_cipher/caesar.py
@@ -4,23 +4,8 @@ https://www.algoexpert.io/questions/Caesar%20Cipher%20Encryptor
 
 from string import ascii_lowercase as lower
 
-def caesar_rotation_v2(string, key):
-    index_letter = {i: letter for i, letter in enumerate(lower, 1)}
-   
-    new_string = [] 
-    while len(new_string) < len(string):
-        #import pdb; pdb.set_trace()
-        for index, letter in index_letter.items():
-            if letter in string:
-                new_index = index + key
-                if new_index > 26:
-                    new_index = new_index % 26
-                new_string.append(index_letter[new_index])
-    return "".join(new_string)
-
-#print(caesar_rotation("iwufqnkqkqoolxzzlzihqfm", 25))
-
 def caesar_rotation(string, key):
+    alphabet_length = 26
     index_letter = {i: letter for i, letter in enumerate(lower, 1)}
    
     new_string = [] 
@@ -28,10 +13,9 @@ def caesar_rotation(string, key):
         for index, char in index_letter.items():
             if letter == char:
                 new_value = index + key
-                if new_value > 26:
-                    new_value = new_value % 26
+                if new_value > alphabet_length:
+                    new_value = new_value % alphabet_length
                 new_string.append(index_letter[new_value])
     return "".join(new_string)
 
 
-#print(caesar_rotation("iwufqnkqkqoolxzzlzihqfm", 25))
