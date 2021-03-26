27ce762e06ad7856fb89bbff54ac9093025e79d1
Amy M Haddad <amy.m.haddad@gmail.com>
2021-03-26 10:40:30 -0400

    Completed problem

--- algoExpert/caesar_cipher/test_caesar.py
+++ algoExpert/caesar_cipher/test_caesar.py
@@ -6,9 +6,8 @@ def test_caesar_rotation():
     assert caesar_rotation("iwufqnkqkqoolxzzlzihqfm", 25) == "hvtepmjpjpnnkwyykyhgpel"
     assert caesar_rotation("abc", 52) == "abc"
 
+
 def test_caesar():
     assert caesar("xyz", 2) == "zab"
-    # assert caesar("iwufqnkqkqoolxzzlzihqfm", 25) == "hvtepmjpjpnnkwyykyhgpel"
-    # assert caesar("abc", 52) == "abc"
-
-
+    assert caesar("iwufqnkqkqoolxzzlzihqfm", 25) == "hvtepmjpjpnnkwyykyhgpel"
+    assert caesar("abc", 52) == "abc"
