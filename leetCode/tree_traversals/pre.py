558ee7eea3441fa685b344bc4d963649d902e663
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-06 06:41:07 -0400

    First commit - pre order works

---
+++ leetCode/tree_traversals/pre.py
@@ -0,0 +1,43 @@
+"""
+https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/
+"""
+from stack import Stack 
+
+class Tree:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+root = Tree(1)
+root.left = Tree(2)
+root.right = Tree(3)
+root.right.left = Tree(4)
+root.right.right = Tree(5)
+# root.left = Tree(2)
+# root.left.left = Tree(3)
+# root.right = Tree(4)
+#
+def pre(root):
+    if root is None:
+        return 
+
+    if root == []:
+        return []
+
+    stack = Stack()
+    stack.push(root)
+    results = []
+
+    while not stack.is_empty():
+        curr_root = stack.pop()
+        results.append(curr_root.val)
+
+        if curr_root.right is not None:
+            stack.push(curr_root.right)
+
+        if curr_root.left is not None:
+            stack.push(curr_root.left)
+
+    return results
+
+print(pre(root))
