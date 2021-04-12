7288885f5477a2bdcd6a160e50d002b428510145
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-12 11:51:42 -0400

    Solved problem

--- leetCode/max_depth/test_max.py
+++ leetCode/max_depth/test_max.py
@@ -1,6 +1,7 @@
 from max import maxDepth
 
+
 def test_maxDepth():
-    assert maxDepth([3,9,20,None, None,15,7]) == 3
+    assert maxDepth([3, 9, 20, None, None, 15, 7]) == 3
     assert maxDepth([]) == 0
     assert maxDepth([0]) == 1
