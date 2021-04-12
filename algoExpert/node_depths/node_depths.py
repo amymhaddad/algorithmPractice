ea882779338eb3cd6b94c554c6d7c6983531ed84
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-12 09:15:25 -0400

    Solved problem

--- algoExpert/node_depths/node_depths.py
+++ algoExpert/node_depths/node_depths.py
@@ -4,11 +4,13 @@ https://www.algoexpert.io/questions/Node%20Depths
 
 from collections import deque
 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
+
 
 root = TreeNode(1)
 root.left = TreeNode(2)
@@ -20,42 +22,49 @@ root.left.right = TreeNode(5)
 root.left.left.left = TreeNode(8)
 root.left.left.right = TreeNode(9)
 
-# root.left = TreeNode(2)
-# root.right = TreeNode(4)
-# root.left.left = TreeNode(3)
+
+# Time complexity - O(n) // Space - O(h)
+# Sol 1
+def nodeDepths(root):
+    stack = [{"node": root, "depth": 0}]
+    depth_count = 0
+
+    while len(stack) > 0:
+        node_info = stack.pop()
+        node, depth = node_info["node"], node_info["depth"]
+        if node is None:
+            continue
+        depth_count += depth
+        stack.append({"node": node.left, "depth": depth + 1})
+        stack.append({"node": node.right, "depth": depth + 1})
+    return depth_count
 
 
-def bfs(root):
+# Sol 2
+def nodeDepths(root):
     if root is None:
-        return 
+        return
+
     que = deque()
-    que.append(root)
-    
+    que.append([0, root])
     total = []
-    head = root 
-    level = 0
-    while True:   
-        root = que.pop()
-        if root == head:
-            total.append(0)
+    while que:
+        node = que.pop()
+        level, root = node[0], node[1]
+        total.append(level)
 
-        if not que:
-            level += 1
-       
-        if root.left is None and root.right is None:
-            
-            return sum(total)
-        
-        else:
-            #level += 1
-            if root.left: 
-                que.appendleft(root.left)
-                total.append(level)
-            if root.right:
-                que.appendleft(root.right)
-                total.append(level)
+        if root.left is not None:
+            que.append([level + 1, root.left])
+        if root.right is not None:
+            que.append([level + 1, root.right])
+    return sum(total)
 
-   # return total 
-print(bfs(root))
-#Need to move the level -- this should only increment when the que is empty
 
+# Sol 3
+def nodeDepths(root, depth=0):
+    if root is None:
+        return 0
+    else:
+        return (
+            depth + nodeDepths(root.left, depth + 1) + nodeDepths(root.right, depth + 1)
+        )
