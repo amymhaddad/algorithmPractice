81b75eb996aa3da4e51c1ce8796bfce8b3f8ab9c
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-20 07:52:11 -0400

    Solved problem -- ready for PR

--- leetCode/range_sum/range_sum.py
+++ leetCode/range_sum/range_sum.py
@@ -1,11 +1,14 @@
 """
 https://leetcode.com/problems/range-sum-of-bst/
 """
-class Tree():
-   def __init__(self, value, left = None, right = None):
-       self.value = value
-       self.left = left
-       self.right = right
+
+
+class Tree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
 
 root = Tree(1)
 root.left = Tree(2)
@@ -13,27 +16,22 @@ root.left.left = Tree(3)
 root.right = Tree(4)
 
 
+def rangeSums(root, low, high):
+    total = []
+    helper(root, low, high, total)
+    return sum(total)
 
-def rangeSumsBST(root, low, high):
-    return helper(root, low, high, 0)
 
-def is_in_range(current_value, low, high):
-    return any(i == current_value for i in range(low, high+1))
-
-def helper(root, low, high, running_total):
+def helper(root, low, high, total):
     if root is None:
-        return 
-    #import pdb; pdb.set_trace() 
-    if is_in_range(root.value, low, high):
-        running_total += root.value
-
+        return
+    is_valid = root.value >= low and root.value <= high
+    if is_valid:
+        total.append(root.value)
     if root.right is None and root.left is None:
-        if is_in_range(root.value, low, high):
-            running_total += root.value
+        return
+    helper(root.left, low, high, total)
+    helper(root.right, low, high, total)
 
-    if root.left:
-        return helper(root.left, low, high, running_total)
 
-    if root.right:
-        return helper(root.right, low, high, running_total)
-print(rangeSumsBST(root, 1, 4))
+print(rangeSums(root, 1, 4))
