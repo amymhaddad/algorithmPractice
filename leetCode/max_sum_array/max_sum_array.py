237357b84b9a57d230e269f8da310c98ba6658aa
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-21 07:29:53 -0400

    first commit

---
+++ leetCode/max_sum_array/max_sum_array.py
@@ -0,0 +1,14 @@
+"""
+https://leetcode.com/problems/maximum-subarray/
+"""
+
+arr = [-2,1,-3,4,-1,2,1,-5,4]
+
+def max_arr(arr):
+    prev = arr[0] 
+    total =0 
+
+    for num in arr:
+        if num 
+
+print(max_arr(arr))
