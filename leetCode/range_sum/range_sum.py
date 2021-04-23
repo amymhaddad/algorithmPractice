c2e29ef475987262408509a816ab9509b0e361af
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-23 06:46:59 -0400

    Added updates from PR

--- leetCode/range_sum/range_sum.py
+++ leetCode/range_sum/range_sum.py
@@ -10,28 +10,37 @@ class Tree:
         self.right = right
 
 
-root = Tree(1)
-root.left = Tree(2)
-root.left.left = Tree(3)
-root.right = Tree(4)
-
-
 def rangeSums(root, low, high):
     total = []
-    helper(root, low, high, total)
+    traverse(root, low, high, total)
+
     return sum(total)
 
 
-def helper(root, low, high, total):
+def traverse(root, low, high, total):
     if root is None:
         return
+
     is_valid = root.value >= low and root.value <= high
+
     if is_valid:
         total.append(root.value)
     if root.right is None and root.left is None:
         return
-    helper(root.left, low, high, total)
-    helper(root.right, low, high, total)
+
+    traverse(root.left, low, high, total)
+    traverse(root.right, low, high, total)
 
 
-print(rangeSums(root, 1, 4))
+def get_range_sums():
+    pass
+
+
+if __name__ == "__main__":
+    get_range_sums()
+    root = Tree(1)
+    root.left = Tree(2)
+    root.left.left = Tree(3)
+    root.right = Tree(4)
+
+    print(rangeSums(root, 1, 4))
