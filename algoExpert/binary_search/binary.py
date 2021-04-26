dd5b6ca0449c8e8ef0a809201780982088be762e
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-26 07:50:45 -0400

    Solved problem

--- algoExpert/binary_search/binary.py
+++ algoExpert/binary_search/binary.py
@@ -2,33 +2,18 @@
 https://www.algoexpert.io/questions/Binary%20Search
 """
 
+
 def binary(array, target):
-
     low = 0
-    high = len(array) 
+    high = len(array) - 1
 
-    while high - low > 1:
-        mid = (high + low) // 2
-        if array[mid] >= target:
-            high = mid
-        
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == target:
+            return mid
+
+        elif array[mid] > target:
+            high = mid - 1
         else:
-            low = mid 
-    #import pdb; pdb.set_trace() 
-    if low == high:
-        return -1
-    elif array[high] == target:
-        return high
-    else:
-        return -1
-
-
-# arr = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
-# target = 0
-
-arr = [0, 1, 21, 33, 45, 45, 61, 71, 72, 73]
-target = 70
-
-# arr = [1, 2, 2, 3, 4]
-# target = 2
-print(binary(arr, target))
+            low = mid + 1
+    return -1
--- algoExpert/binary_search/test_binary.py
+++ algoExpert/binary_search/test_binary.py
@@ -1,5 +1,6 @@
 from binary import binary
 
+
 def test_binary():
-    assert binary([0, 1, 21, 33, 45, 45, 61, 71, 72, 73],33) == 3
+    assert binary([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 33) == 3
     assert binary([0, 1, 21, 33, 45, 45, 61, 71, 72, 73], 0) == 0
