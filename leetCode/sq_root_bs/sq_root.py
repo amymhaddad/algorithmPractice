fe5e88a710015495235cd8f01f92682e5a7882b5
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-27 07:52:15 -0400

    Added new algorithm

--- leetCode/sq_root_bs/sq_root.py
+++ leetCode/sq_root_bs/sq_root.py
@@ -3,33 +3,56 @@ https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/
 """
 #update var names
 def sq_root(num):
+    if num <= 2:
+        return num
     low = 1
     high = num 
 
     while low < high:
         curr = (low + high) // 2
-        #import pdb; pdb.set_trace()
+        import pdb; pdb.set_trace()
         if curr * curr == num or in_range(curr, num):
             return curr
         elif curr * curr > num: 
             high = curr - 1
         else:
             low = curr + 1
-    return -1
+    return 1
 
 def in_range(num, given):
     res = []
-    
-    #import pdb; pdb.set_trace()
     lower = num * num 
     upper = (num + 1) * (num + 1)
     for i in range(lower, upper):
         res.append(i)
     return given in res
 
-#print(lower, upper)
 
-print(sq_root(8))
+#print(sq_root(5))
+
+def v2_sq_root(num):
+    if num <= 2:
+        return num 
+    low = 1 
+    high = num 
+
+    while high - low > 1:
+        mid = (low + high) // 2
+
+        if mid * mid >= num:
+            high = mid
+        else:
+            low = mid
+    #import pdb; pdb.set_trace()
+    if mid * mid == num:
+        return mid
+    elif num >= low * low and num <= high * high:
+        return low 
+    
+    
+
+
+print(v2_sq_root(5))
 
 
 
--- leetCode/sq_root_bs/test_sq_root.py
+++ leetCode/sq_root_bs/test_sq_root.py
@@ -1,7 +1,8 @@
-from sq_root import sq_root
+from sq_root import v2_sq_root
 
 
 def test_sq_root():
     assert sq_root(4) == 2
     assert sq_root(9) == 3
     assert sq_root(8) == 2
+    assert sq_root(0) == 0
