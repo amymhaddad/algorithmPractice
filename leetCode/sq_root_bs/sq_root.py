f1f419f042c4510c370a01d4dda4304afa995d01
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-27 07:20:30 -0400

    First attempt at algo

--- leetCode/sq_root_bs/sq_root.py
+++ leetCode/sq_root_bs/sq_root.py
@@ -1,34 +1,35 @@
 """
 https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/
 """
-
+#update var names
 def sq_root(num):
-    low = 0
-    high = num + 1
+    low = 1
+    high = num 
 
     while low < high:
         curr = (low + high) // 2
-
-        if curr * curr == num or num in in_range(curr):
+        #import pdb; pdb.set_trace()
+        if curr * curr == num or in_range(curr, num):
             return curr
-        elif num > curr:
+        elif curr * curr > num: 
             high = curr - 1
         else:
             low = curr + 1
     return -1
 
-def in_range(num):
+def in_range(num, given):
     res = []
-
+    
+    #import pdb; pdb.set_trace()
     lower = num * num 
     upper = (num + 1) * (num + 1)
     for i in range(lower, upper):
         res.append(i)
-    return num in res
+    return given in res
 
 #print(lower, upper)
 
-print(sq_root(4))
+print(sq_root(8))
 
 
 
---
+++ leetCode/sq_root_bs/test_sq_root.py
@@ -0,0 +1,7 @@
+from sq_root import sq_root
+
+
+def test_sq_root():
+    assert sq_root(4) == 2
+    assert sq_root(9) == 3
+    assert sq_root(8) == 2
