149941edfb79ed70ca7945df3c9f764ec2fb9507
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-27 07:59:28 -0400

    Solved it

--- leetCode/sq_root_bs/sq_root.py
+++ leetCode/sq_root_bs/sq_root.py
@@ -2,33 +2,6 @@
 https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/
 """
 #update var names
-def sq_root(num):
-    if num <= 2:
-        return num
-    low = 1
-    high = num 
-
-    while low < high:
-        curr = (low + high) // 2
-        import pdb; pdb.set_trace()
-        if curr * curr == num or in_range(curr, num):
-            return curr
-        elif curr * curr > num: 
-            high = curr - 1
-        else:
-            low = curr + 1
-    return 1
-
-def in_range(num, given):
-    res = []
-    lower = num * num 
-    upper = (num + 1) * (num + 1)
-    for i in range(lower, upper):
-        res.append(i)
-    return given in res
-
-
-#print(sq_root(5))
 
 def v2_sq_root(num):
     if num <= 2:
@@ -38,21 +11,28 @@ def v2_sq_root(num):
 
     while high - low > 1:
         mid = (low + high) // 2
-
         if mid * mid >= num:
             high = mid
         else:
             low = mid
-    #import pdb; pdb.set_trace()
-    if mid * mid == num:
-        return mid
+    if high * high == num:
+        return high
     elif num >= low * low and num <= high * high:
         return low 
     
     
 
 
-print(v2_sq_root(5))
+print(v2_sq_root(9))
+
+"""
+Key points:
+-Set bounds w/2 pointers
+-Set a condition that'll leave me w/2 values
+-Set a question -- looking for the first Y value 
+-Post-process
+
+"""
 
 
 
--- leetCode/sq_root_bs/test_sq_root.py
+++ leetCode/sq_root_bs/test_sq_root.py
@@ -2,7 +2,7 @@ from sq_root import v2_sq_root
 
 
 def test_sq_root():
-    assert sq_root(4) == 2
-    assert sq_root(9) == 3
-    assert sq_root(8) == 2
-    assert sq_root(0) == 0
+    assert v2_sq_root(4) == 2
+    assert v2_sq_root(9) == 3
+    assert v2_sq_root(8) == 2
+    assert v2_sq_root(0) == 0
