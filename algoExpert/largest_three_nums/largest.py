a8f7f1875bf8b6a6b2635131fe9a62fc13a1c5f9
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-28 07:12:51 -0400

    First algo

--- algoExpert/largest_three_nums/largest.py
+++ algoExpert/largest_three_nums/largest.py
@@ -4,7 +4,7 @@ https://www.algoexpert.io/questions/Find%20Three%20Largest%20Numbers
 
 def find_largest_three_nums(array):
     if len(array) == 3:
-        return array
+        return sort_array(array)
     
     largest_nums = []
 
@@ -13,10 +13,22 @@ def find_largest_three_nums(array):
         index_of_largest = array.index(largest)
         largest_nums.append(largest)
         array.pop(index_of_largest)
-    return largest_nums
+    return sort_array(largest_nums)
 
 
-array = [10, 5, 9, 10, 12]
-print(find_largest_three_nums(array))
+
+array = [141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]
+#print(find_largest_three_nums(array))
+
+def sort_arr(array):
+    max_index_so_far = 0
+
+    for i in range(len(array[1:])):
+        if array[i] < array[max_index_so_far]:
+            max_index_so_far = i 
+
+    return max_index_so_far
+
+print(sort_arr([141, 18, 541]))
 
 
