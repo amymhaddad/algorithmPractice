7a25ee0356e28a8a57fecb932c88dfb2b591b79c
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-01 07:29:28 -0400

    Solved it

--- algoExpert/class_photo/class_photo.py
+++ algoExpert/class_photo/class_photo.py
@@ -2,6 +2,7 @@
 https://www.algoexpert.io/questions/Class%20Photos
 """
 
+
 def classPhotos(redShirtHeights, blueShirtHeights):
     redShirtHeights.sort()
     blueShirtHeights.sort()
@@ -9,15 +10,12 @@ def classPhotos(redShirtHeights, blueShirtHeights):
     if len(redShirtHeights) != len(blueShirtHeights):
         return False
 
-    taller_student = max(redShirtHeights[0], blueShirtHeights[0])
-    if taller_student in blueShirtHeights:
-        back_row = blueShirtHeights
-        front_row = redShirtHeights
-    else:
-        back_row = redShirtHeights
-        front_row = blueShirtHeights
+    first_tallest_student = max(redShirtHeights[0], blueShirtHeights[0])
+    students = arrange_students_in_rows(
+        redShirtHeights, blueShirtHeights, first_tallest_student
+    )
+    back_row, front_row = students[0], students[1]
 
-    #import pdb; pdb.set_trace()
     for i, height in enumerate(back_row):
         invalid_height = back_row[i] <= front_row[i]
 
@@ -25,7 +23,13 @@ def classPhotos(redShirtHeights, blueShirtHeights):
             return False
     return True
 
-redShirtHeights = [6, 9, 2, 4, 5, 1]
-blueShirtHeights = [5, 8, 1, 3, 4, 9]
 
-print(classPhotos(redShirtHeights, blueShirtHeights))
+def arrange_students_in_rows(redShirtHeights, blueShirtHeights, first_tallest_student):
+    first_tallest_student = max(redShirtHeights[0], blueShirtHeights[0])
+    if first_tallest_student == blueShirtHeights[0]:
+        back_row = blueShirtHeights
+        front_row = redShirtHeights
+    else:
+        back_row = redShirtHeights
+        front_row = blueShirtHeights
+    return back_row, front_row
