fc1c9e3b67de621ee7c72d39f20e94700b0873f2
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-01 10:11:55 -0400

    Solved it

--- leetCode/min_operations/min_operations.py
+++ leetCode/min_operations/min_operations.py
@@ -2,16 +2,17 @@
 https://leetcode.com/problems/minimum-operations-to-make-the-array-increasing/
 """
 
+
 def min_operations(nums):
 
     prev_num = nums[0]
     total_operations = 0
 
-    for i, curr_num in enumerate([1:]):
+    for i, curr_num in enumerate(nums[1:]):
         if curr_num > prev_num:
+            prev_num = curr_num
             continue
-        curr_operations = curr_num - prev_num
+        curr_operations = (prev_num - curr_num) + 1
         total_operations += curr_operations
-        prev_num = curr_num
+        prev_num = curr_num + curr_operations
     return total_operations
-
--- leetCode/min_operations/test_min_operations.py
+++ leetCode/min_operations/test_min_operations.py
@@ -1,4 +1,5 @@
 from min_operations import min_operations
 
+
 def test_min_operations():
     assert min_operations([1, 5, 2, 4, 1]) == 14
