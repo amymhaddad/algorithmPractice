16ee9481329fcae813308ca49ec3ee932305ab5f
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-04 08:04:14 -0400

    WIP v2 implementation

--- algoExpert/delete_kth_ll/delete.py
+++ algoExpert/delete_kth_ll/delete.py
@@ -23,7 +23,7 @@ def removeKthNodeFromEnd(head, k):
        # import pdb; pdb.set_trace()
         if head.next is None:
             head = head.next
-            break
+#            break
        
         elif head.next.value == val_to_remove:
             head = head.next.next
@@ -38,11 +38,57 @@ a.next = b
 b.next = c
 c.next = d 
 
+
 k = 2
 
-update = removeKthNodeFromEnd(a, k)
 
-while update is not None:
-    print(update.value)
-    update = update.next
+def remove(head, k):
+    first = head
+    second = head
+    counter = 1 
+
+    while counter <= k:
+        second = second.next
+        counter += 1
+   
+    prev = first
+    while second is not None:
+        if second.next is None:
+            prev.next = second
+            break
+        second = second.next
+        first = first.next
+        prev = first
+    return prev
   
+#update = remove(a, k)
+
+def v2_remove(head, k):
+    first = head
+    second = head
+    counter = 1 
+
+    #want to be inclusive -- move k spots so second is k spots ahead of first
+    while counter <= k:
+        second = second.next
+        counter += 1
+
+    #check if second is already at None
+    if second is None:
+        head.value = head.next.value
+        head.next = head.next.next
+        return
+    
+    #Set this condition w/second.next so second.value points to the last 
+    #value in the LL -- not to None. This means that first points to the value
+    #right before the one I need to delete
+    while second.next is not None:
+        second = second.next
+        first = first.next 
+    
+    import pdb; pdb.set_trace()
+
+print(v2_remove(a, 2))
+
+   
+ 
