76e25ece45d93844adc18832ab1800d584dbd7f9
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-06 08:51:00 -0400

    solved it

--- algoExpert/tandem/tandem.py
+++ algoExpert/tandem/tandem.py
@@ -2,33 +2,28 @@
 https://www.algoexpert.io/questions/Tandem%20Bicycle
 """
 
-def v1_tandem(red, blue, fastest):
-    total = []
 
-    for i in range(len(red)):
-        for j in range(len(blue)):
-            max_per_pair = (red[i], blue[i])
-            total.append(max_per_pair)
-    return total
-
-
-red = [5, 5, 3, 9, 2]
-blue = [3, 6, 7, 2, 1]
-fastest = True
-
-def tandem(red, blue, fastest):
-    if fastest:
-        return find_max_speeds(red, blue)
-    return find_min_speeds(red, blue)
-
-def find_max_speeds(red, blue):
-    total = 0 
-    red.sort(reverse=True)
-    blue.sort()
-
+def calculate_speed(red, blue):
+    total = 0
     for i in range(len(red)):
         max_speed = max(red[i], blue[i])
         total += max_speed
     return total
 
-print(find_max_speeds(red, blue))
+
+def find_max_speeds(red, blue):
+    red.sort(reverse=True)
+    blue.sort()
+    return calculate_speed(red, blue)
+
+
+def find_min_speeds(red, blue):
+    red.sort()
+    blue.sort()
+    return calculate_speed(red, blue)
+
+
+def tandem(red, blue, fastest):
+    if fastest:
+        return find_max_speeds(red, blue)
+    return find_min_speeds(red, blue)
--- algoExpert/tandem/test_tandem.py
+++ algoExpert/tandem/test_tandem.py
@@ -1,5 +1,6 @@
 from tandem import tandem
 
+
 def test_tandem():
     assert tandem([5, 5, 3, 9, 2], [3, 6, 7, 2, 1], True) == 32
     assert tandem([5, 5, 3, 9, 2], [3, 6, 7, 2, 1], False) == 11
