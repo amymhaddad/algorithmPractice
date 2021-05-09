fea7e47d44caa8fe51717108d3ba629fdb282b4d
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-09 07:25:46 -0400

    Updated first iteration of algo

--- algoExpert/bubble_sort/bubble.py
+++ algoExpert/bubble_sort/bubble.py
@@ -2,16 +2,13 @@
 https://www.algoexpert.io/questions/Bubble%20Sort
 """
 
-array = [8, 5, 2, 9, 5, 6, 3]
+array = [8, 9, 3]
 def smallest_index(array):
-    smallest_index_so_far = 0 
- 
-    for i in range(len(array[1:])):
-        curr_val = array[i]
-        prev_val = array[smallest_index_so_far]
-        if curr_val < prev_val:
-            smallest_index_so_far = i
-    return smallest_index_so_far
+    smallest_index = 0 
+    for i, num in enumerate(array[1:], 1):
+        if array[i] < array[smallest_index]:
+            smallest_index = i
+    return smallest_index
 
 def sort_array(array):
     new_arr = []
