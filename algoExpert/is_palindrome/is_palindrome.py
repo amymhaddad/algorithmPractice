7ec88678dcedca2698b3d8c92eb346babd308858
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-17 12:03:26 -0400

    Solved it

--- algoExpert/is_palindrome/is_palindrome.py
+++ algoExpert/is_palindrome/is_palindrome.py
@@ -2,11 +2,21 @@
 https://www.algoexpert.io/questions/Palindrome%20Check
 """
 
+
 def is_palendrome(string):
-    if len(string) == 1:
+    if len(string) <= 1:
         return True
 
     return string[0] == string[-1] and is_palendrome(string[1:-1])
 
-string = "abaa"
-print(is_palendrome(string))
+
+def v2_is_palendrome(string):
+    p1 = 0
+    p2 = len(string) - 1
+
+    while p2 - p1 >= 1:
+        if string[p1] != string[p2]:
+            return False
+        p1 += 1
+        p2 -= 1
+    return True
