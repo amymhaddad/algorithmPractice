bf629e2aaa94db6e88eed1d726edd899d4f3b73c
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-21 09:50:45 -0400

    Final algo

--- algoExpert/task_assignment/task.py
+++ algoExpert/task_assignment/task.py
@@ -15,9 +15,11 @@ def task_assignment(k, tasks):
     task_assignments = []
     
     for i in range(k):
-        task_assignments.append(tasks[lower])
-        task_assignments.append(tasks[upper])
-        #import pdb; pdb.set_trace()
+        lower_val = tasks[lower]
+        upper_val = tasks[upper]
+        task_assignments.extend([[lower_val, upper_val]])
+        # task_assignments.append(tasks[lower])
+        # task_assignments.append(tasks[upper])
         lower += 1
         upper -= 1
     return get_indices(task_assignments, task_index)
@@ -25,12 +27,20 @@ def task_assignment(k, tasks):
 
 def get_indices(task_assignments, task_index):
     indices = []
-    #import pdb; pdb.set_trace()
+    
+    original_tasks = [task for task in task_index.values()]
     
     for tasks in task_assignments:
-        first_task = tasks[0]
-        second_task = tasks[1]
-        indices.extend([[task_index[first_task], task_index[second_task]]])
+        pair = []
+        for task in tasks:
+            if task in original_tasks:
+                #import pdb; pdb.set_trace()
+                task_index = original_tasks.index(task)
+                pair.append(task_index) 
+                original_tasks[task_index] = None
+        indices.extend([pair]) 
+            
+
     return indices
 
 
