1b4c3a71861b18661d92e8967c6353e9a12b35a1
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-25 09:41:05 -0400

    Solved it

--- algoExpert/bst/bst.py
+++ algoExpert/bst/bst.py
@@ -3,11 +3,13 @@ https://www.algoexpert.io/questions/Validate%20BST
 """
 import math
 
+
 class TreeNode:
-   def __init__(self, value=0, left=None, right=None):
-       self.value = value
-       self.left = left
-       self.right = right
+    def __init__(self, value=0, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
 
 root = TreeNode(10)
 root.left = TreeNode(5)
@@ -16,16 +18,13 @@ root.left.left = TreeNode(2)
 root.left.right = TreeNode(5)
 
 
-def validateBst(tree, high=math.inf, low=-math.inf):
-    if tree.right is None and tree.left is None:
-        return True 
+def validateBst(tree, low=-math.inf, high=math.inf):
+    if tree is None:
+        return True
 
-    if tree.value < low or tree.value > high:
-         return False
-    #import pdb; pdb.set_trace()
-    if tree.value <= high and tree.value > low:
-        validateBst(tree.left, high=tree.value, low=low)
+    if tree.value < low or tree.value >= high:
+        return False
 
-    if tree.value > low and tree.value <= high:
-        validateBst(tree.right, high=high, low = tree.value)
-print(validateBst(root))
+    return validateBst(tree.left, low, tree.value) and validateBst(
+        tree.right, tree.value, high
+    )
