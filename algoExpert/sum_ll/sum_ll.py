6b73abb9ca6d4ba661f8daf978175894c948526f
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-27 09:45:38 -0400

    Added while loop to iterate through both lists

--- algoExpert/sum_ll/sum_ll.py
+++ algoExpert/sum_ll/sum_ll.py
@@ -2,28 +2,53 @@
 https://www.algoexpert.io/questions/Sum%20of%20Linked%20Lists
 """
 
-class Link:
+class LinkedList:   
     def __init__(self, value):
         self.value = value
         self.next = None
-#x = Link(1, Link(5))
-#y = Link(1, Link(4))
 
 def sumOfLinkedLists(linkedListOne, linkedListTwo):
-
     linked1_ints = get_digits(linkedListOne)
+    linked2_ints = get_digits(linkedListTwo)
+    sum_of_lists = linked1_ints + linked2_ints
 
-    print(linked1_ints)
+    new_list = LinkedList(sum_of_lists % 10)
+    sum_of_lists = sum_of_lists // 10
+
+    while sum_of_lists is not None:
+        last_digit = sum_of_lists % 10 
+        new_list.value, new_list.next = last_digit, new_list(new_list.first, ())
+        sum_of_lists = sum_of_lists // 10
+
+    return new_list
+    
+
 
 
 def get_digits(linked_list):
 
-    digits = []
+    total_numbers = []
 
     while linked_list is not None:
-        linked_list = linked_list.value
-        digits.append(linked_list.value)
+        number = linked_list.value
+        total_numbers.append(str(number))
         linked_list = linked_list.next
-    return digits
+    return reverse_digits(total_numbers)
 
-print(sumOfLinkedLists(x, y))
+def reverse_digits(digits):
+    i = 0
+    j = len(digits) -1
+
+    while i < j:
+        digits[i], digits[j] = digits[j], digits[i]
+        i += 1
+        j -= 1
+    return int("".join(digits))
+
+
+nums = 123
+
+while nums > 0:
+    print("here")
+
+    nums = nums // 10
