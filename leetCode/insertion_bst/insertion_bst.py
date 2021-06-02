b386c29bb26f7998fa42eb7bb53f5e887669ff5c
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-02 10:02:07 -0400

    Solved it

--- leetCode/insertion_bst/insertion_bst.py
+++ leetCode/insertion_bst/insertion_bst.py
@@ -1,19 +1,21 @@
 """
 https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/141/basic-operations-in-a-bst/1003/
 """
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
-#Iterative solution
+
 class Solution:
     def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
         current = root
         if current is None:
             return TreeNode(val)
-        
+
         while True:
             if val < current.val:
                 if current.left is None:
