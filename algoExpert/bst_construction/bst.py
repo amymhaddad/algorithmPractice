192a5c35ef00a3295863de3809e39901a0477160
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-05 09:01:25 -0400

    Completed problem

--- algoExpert/bst_construction/bst.py
+++ algoExpert/bst_construction/bst.py
@@ -2,13 +2,13 @@
 https://www.algoexpert.io/questions/BST%20Construction
 """
 
-#Iterative approach
+# Iterative approach
 class BST:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-    
+
     def insert(self, value):
         current = self
 
@@ -16,13 +16,13 @@ class BST:
             if value < current.value:
                 if current.left is None:
                     current.left = BST(value)
-                    break 
+                    break
                 else:
-                    current = current.left 
+                    current = current.left
             else:
                 if current.right is None:
                     current.right = BST(value)
-                    break 
+                    break
                 else:
                     current = current.right
 
@@ -39,15 +39,16 @@ class BST:
         return False
 
 
-#Recursive approach
+#
+# Recursive approach
 class BST:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-    
+
     def insertion(self, value):
-        if value < self.value:
+        if self.value > value:
             if self.left is None:
                 self.left = BST(value)
             else:
@@ -57,3 +58,34 @@ class BST:
                 self.right = BST(value)
             else:
                 self.right.insertion(value)
+
+    def contains(self, value):
+        if self.value > value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(value)
+        elif self.value < value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(value)
+        else:
+            return True
+
+
+a = BST(10)
+a.left = BST(5)
+a.right = BST(15)
+a.insertion(12)
+print(a.contains(112))
+
+
+def pre_order(a):
+    if a:
+        print(a.value)
+        pre_order(a.left)
+        pre_order(a.right)
+
+
+print(pre_order(a))
