12e84ea671bf07e36d738a6cc0b6b3c38910ba36
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-18 10:16:06 -0400

    Added logic to rotate the array of indices

--- algoExpert/starting_city/starting_city.py
+++ algoExpert/starting_city/starting_city.py
@@ -2,25 +2,22 @@
 https://www.algoexpert.io/questions/Valid%20Starting%20City
 """
 
-arr = [1,2,3,4,5,6,7] 
-k = 3
-def rotate_array(arr, k):
-    arr_length = len(arr)
-    nums = len(arr) * [None]
 
-    for i, value in enumerate(arr):
+
+
+
+
+
+def rotate_index(indices, step, curr_index):
+    indices_length = len(indices)
+    new_indices = indices_length * [None]
+
+    for i, value in enumerate(indices):
+        index = (i + step ) % indices_length
         import pdb; pdb.set_trace()
-        index = (i + k) % arr_length
-        nums[index] = value
-    return nums
-    
-def rotate_index(arr, step, curr_index):
-    indices = [i for i in range(len(arr))]
-    print(indices)
-    arr_length = len(arr)
-    new_indices = arr_length * [None]
+        new_indices[i] = index
+    return new_indices
 
 distances = [5, 25, 15, 10, 15]
 step = 1
-curr_index = 0
-print(rotate_index(distances, step, curr_index))
+#print(rotate_index(distances, step, curr_index))
