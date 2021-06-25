609a95cec18054f58c3531810128bff72a9c7bd3
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-25 12:03:17 -0400

    WIP - iterative sol

--- algoExpert/smallest_diff/smallest_diff.py
+++ algoExpert/smallest_diff/smallest_diff.py
@@ -25,11 +25,25 @@ def smallest_difference(arr1, arr2):
     smallest_diff = min([value for value in results.keys()])
     return results[smallest_diff]
 
-arr1 = [10, 0, 20, 25]
-arr2 = [1005, 1006, 1014, 1032, 1031]
-
+# arr1 = [10, 0, 20, 25]
+# arr2 = [1005, 1006, 1014, 1032, 1031]
+#
 # arr1 = [-1, 5, 10, 20, 28, 3]
 # arr2 = [26, 134, 135, 15, 17]
-print(smallest_difference(arr1, arr2))
-            
+
+arr1 = [0, 20]
+arr2 = [21, -2]
+
+def v2smalest(arr1, arr2):
+    results = {}
+
+    for i in range(len(arr1)):
+        for k in range(i + 1, len(arr1)):
+           #import pdb; pdb.set_trace()
+            abs_diff = abs(arr1[i] - arr2[k])
+            results[abs_diff] = [arr1[i], arr2[k]]
+    import pdb; pdb.set_trace()
+    smallest_diff = min([value for value in results.keys()])
+    return results[smallest_diff]
+print(v2smalest(arr1, arr2))
 
