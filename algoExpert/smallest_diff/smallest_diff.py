6be81b1f94a5ee634d4961ae71fc7005e25c6832
Amy M Haddad <amy.m.haddad@gmail.com>
2021-06-26 12:15:31 -0400

    Completed problem

--- algoExpert/smallest_diff/smallest_diff.py
+++ algoExpert/smallest_diff/smallest_diff.py
@@ -2,50 +2,31 @@
 https://www.algoexpert.io/questions/Smallest%20Difference
 """
 
-def smallest_difference(arr1, arr2):
+
+def smallest(arr1, arr2):
     arr1.sort()
     arr2.sort()
+    index_1 = 0
+    index_2 = 0
+    smallest_diff = float("inf")
+    current_diff = float("inf")
+    pairs = []
 
-    left = len(arr1) -1
-    right = 0
-    
-    min_diff_so_far = abs(arr1[left]) - abs(arr2[right])
+    while index_1 < len(arr1) and index_2 < len(arr2):
+        first_num = arr1[index_1]
+        second_num = arr2[index_2]
 
-    results = {}
-    results[min_diff_so_far] = [arr1[left], arr2[right]]
+        if first_num < second_num:
+            current_diff = second_num - first_num
+            index_1 += 1
 
-    for i in range(len(arr2)):
-        abs_diff = abs(arr1[left] - (arr2[i])) - 0
-        if abs_diff > min_diff_so_far:
-            break 
-        if abs_diff < min_diff_so_far:
-            min_diff_so_far = abs_diff
-            results[min_diff_so_far] = [arr1[left], arr2[i]]
+        elif first_num > second_num:
+            current_diff = first_num - second_num
+            index_2 += 1
+        else:
+            return [first_num, second_num]
 
-    smallest_diff = min([value for value in results.keys()])
-    return results[smallest_diff]
-
-# arr1 = [10, 0, 20, 25]
-# arr2 = [1005, 1006, 1014, 1032, 1031]
-#
-#arr1 = [-1, 5, 10, 20, 28, 3]
-#arr2 = [26, 134, 135, 15, 17]
-
-arr1 = [0, 20]
-arr2 = [21, -2]
-
-def v2smalest(arr1, arr2):
-    results = {}
-    max_arr = max(arr1, arr2)
-    min_arr = min(arr1, arr2)
-
-    for i in range(len(arr1)):
-        for k in range(len(arr2)):
-            print(i, k)
-            abs_diff = abs(arr1[i] - arr2[k])
-            results[abs_diff] = [arr1[i], arr2[k]]
-#    import pdb; pdb.set_trace()
-    smallest_diff = min([value for value in results.keys()])
-    return results[smallest_diff]
-print(v2smalest(arr1, arr2))
-
+        if smallest_diff > current_diff:
+            smallest_diff = current_diff
+            pairs = [first_num, second_num]
+    return pairs
