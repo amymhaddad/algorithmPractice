9f014bf5b301c077dd70279954dd96bfb29b4119
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-26 10:20:56 -0400

    Updated first part of algorithm

--- algoExpert/non_constructible_change/change.py
+++ algoExpert/non_constructible_change/change.py
@@ -3,20 +3,34 @@ def change(coins):
     coins.sort()
 
     change_options = []
+    total_so_far = 0
+    for i in range(len(coins)):
+        if coins[i] not in change_options:
+            change_options.append(coins[i])
 
-    for i, num in enumerate(coins):
-        if num not in change_options:
-            change_options.append(num)
-
-        if len(coins) -1 == i:
-            break
-        next_sum = coins[i]+coins[i+1]
-
-        if next_sum not in change_options:
-            change_options.append(next_sum)
-
+        if i == len(coins) + 1:
+            break 
+        
+        next_val = total_so_far + coins[i]
+        if next_val not in change_options:
+            change_options.append(next_val)
+        total_so_far += coins[i]
+    
     change_options.sort()
-    return change_options
+    for i in range(len(change_options)):
+        if i == len(change_options) - 1:
+            return change_options[-1] + 1
+       
+        elif change_options[i+1] - change_options[i] > 1:
+            max_change_num = change_options[i+1]
+            break 
 
-coins = [1, 2, 4, 9]
+        else:
+            continue
+    print(change_options)
+    #NOW iterate through change_options and get the difference
+    #IF the diff is > 1, check if the diff is alrady in change_options
+    #If not, return the diff at that point
+
+coins = [5, 7, 1, 1, 2, 3, 22]
 print(change(coins))
