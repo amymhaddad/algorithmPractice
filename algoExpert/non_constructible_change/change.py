fd2581bfe25758be3f0a3188d095f91f8424a7e7
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-27 10:32:46 -0400

    Update to algorithm

--- algoExpert/non_constructible_change/change.py
+++ algoExpert/non_constructible_change/change.py
@@ -3,9 +3,6 @@ def change(coins):
     change = 0
 
     for i, val in enumerate(coins):
-        if i == 0 and val != 1:
-            return 1
-
         if val > change + 1:
             return change + 1
 
