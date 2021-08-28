2d475c261ec7ee3ccab8f51ed8d6322967c9e682
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-28 10:09:54 -0400

    Solved it

--- algoExpert/go/first_non_repeating/non_repeating.go
+++ algoExpert/go/first_non_repeating/non_repeating.go
@@ -1,9 +1,8 @@
 package main
 
-import "fmt"
-
 //FirstNonRepeatingCharacter takes a string and returns the index of the first
 //non-repeating character
+//Solution 1
 func FirstNonRepeatingCharacter(str string) int {
 	var letterCounts = map[rune]int{}
 
@@ -14,10 +13,30 @@ func FirstNonRepeatingCharacter(str string) int {
 			letterCounts[char] = 1
 		}
 	}
+
+	for i, char := range str {
+		if letterCounts[char] == 1 {
+			return i
+		}
+	}
 	return -1
 }
 
-func main() {
-	x := FirstNonRepeatingCharacter("abcdcaf")
-	fmt.Println(x)
+//Solution 2
+func FirstNonRepeatingCharacter(str string) int {
+	for index1 := range str {
+		foundDup := false
+		for index2 := range str {
+			if str[index1] == str[index2] && index1 != index2 {
+				foundDup = true
+			}
+		}
+
+		if !foundDup {
+			return index1
+		}
+
+	}
+	return -1
+
 }
