71509352cbd9506bb8930fe51d92aaa3432aba53
Amy M Haddad <amy.m.haddad@gmail.com>
2021-08-31 07:59:36 -0400

    Algo in process

--- algoExpert/go/validateBST/validate.go
+++ algoExpert/go/validateBST/validate.go
@@ -9,11 +9,28 @@ import (
 
 type BST struct {
 	Value int
-
 	Left  *BST
 	Right *BST
 }
 
+func (tree *BST) ValidateBst() bool {
+	max := int(math.MaxInt64)
+	min := int(math.MinInt64)
+
+	return Validate(tree, max, min)
+}
+
+func Validate(tree *BST, max int, min int) bool {
+	if tree == nil {
+		return true
+	}
+	//Valid bst val: val >= min min and val < max val
+	//SO to break these conditions, use the syntax below
+	if tree.Value < min || tree.Value >= max {
+		return false
+	}
+	return Validate(tree.Left, min, tree.Value) && Validate(tree.Right, tree.Value, max)
+}
 
 func main() {
 	x := int(math.Inf(0))
