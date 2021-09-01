69b84c027cfe45067e2397c246466b529b3df90f
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-01 10:27:35 -0400

    Solved it

--- algoExpert/go/validateBST/validate.go
+++ algoExpert/go/validateBST/validate.go
@@ -1,40 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
-// func Inf(sign int) float64
-
+//BST is a tree
 type BST struct {
 	Value int
 	Left  *BST
 	Right *BST
 }
 
+//ValidateBst determines if a given tree is a valid BST
 func (tree *BST) ValidateBst() bool {
 	max := int(math.MaxInt64)
 	min := int(math.MinInt64)
-
-	return Validate(tree, max, min)
+	return validate(tree, min, max)
 }
 
-func Validate(tree *BST, max int, min int) bool {
+func validate(tree *BST, min, max int) bool {
+
 	if tree == nil {
 		return true
 	}
-	//Valid bst val: val >= min min and val < max val
-	//SO to break these conditions, use the syntax below
+
 	if tree.Value < min || tree.Value >= max {
 		return false
 	}
 	return Validate(tree.Left, min, tree.Value) && Validate(tree.Right, tree.Value, max)
-}
-
-func main() {
-	x := int(math.Inf(0))
-	y := 9
-	fmt.Println(x > y)
-	fmt.Println(x)
 }
