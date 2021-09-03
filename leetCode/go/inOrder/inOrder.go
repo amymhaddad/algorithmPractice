bdd4f93675c961906b6eb3686ac70ea7c23ba067
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-03 10:29:33 -0400

    Added recursive solution; working on iterative solution

--- leetCode/go/inOrder/inOrder.go
+++ leetCode/go/inOrder/inOrder.go
@@ -1,11 +1,48 @@
 package main
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// //Recursive solution
+// func inorderTraversal(root *TreeNode) []int {
+// 	var nodes []int
+// 	return getNodes(root, &nodes)
+// }
+
+// func getNodes(root *TreeNode, nodes *[]int) []int {
+
+// 	if root != nil {
+// 		getNodes(root.Left, nodes)
+// 		*nodes = append(*nodes, root.Val)
+// 		getNodes(root.Right, nodes)
+// 	}
+// 	return *nodes
+// }
+
+//Iterative Solution
 func inorderTraversal(root *TreeNode) []int {
-	var nodes []int
-	if root != nil {
-		inorderTraversal(root.Left)
-		nodes = append(nodes, root.val)
-		inorderTraversal(root.Right)
+	//s needs to be slice of pointers to TreeNodes --> *TreeNode
+	//Appending to a slcie of pointers to TreeNodes
+	var s []int
+	var results []int
+
+	for len(s) != 0 {
+		if root.Left != nil {
+			root = root.Left
+			s = append(s, root)
+		}
+
+		results = append(results, root.Val)
+
+		if root.Right != nil {
+			root = root.Right
+			s = append(s, root)
+		}
+
 	}
-	return nodes
+	return results
+
 }
