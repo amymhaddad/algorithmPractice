966f34affc57b22ab7e496a018066dcb1743b255
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-08 06:52:26 -0400

    Added two solutions

--- leetCode/go/inOrder/inOrder.go
+++ leetCode/go/inOrder/inOrder.go
@@ -1,34 +1,32 @@
 package main
 
+//TreeNode is a binary tree
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
 	Right *TreeNode
 }
 
-// //Recursive solution
-// func inorderTraversal(root *TreeNode) []int {
-// 	var nodes []int
-// 	return getNodes(root, &nodes)
-// }
-
-// func getNodes(root *TreeNode, nodes *[]int) []int {
-
-// 	if root != nil {
-// 		getNodes(root.Left, nodes)
-// 		*nodes = append(*nodes, root.Val)
-// 		getNodes(root.Right, nodes)
-// 	}
-// 	return *nodes
-// }
-
-//Iterative Solution
+//Recursive solution
 func inorderTraversal(root *TreeNode) []int {
-	//s needs to be slice of pointers to TreeNodes --> *TreeNode
-	//Appending to a slcie of pointers to TreeNodes
+	var nodes []int
+	return getNodes(root, &nodes)
+}
+
+func getNodes(root *TreeNode, nodes *[]int) []int {
+
+	if root != nil {
+		getNodes(root.Left, nodes)
+		*nodes = append(*nodes, root.Val)
+		getNodes(root.Right, nodes)
+	}
+	return *nodes
+}
+
+//Iterative solution
+func inorderTraversal(root *TreeNode) []int {
 	var s []*TreeNode
 	var results []int
-	var val int 
 
 	for {
 		if root != nil {
@@ -38,14 +36,13 @@ func inorderTraversal(root *TreeNode) []int {
 			if len(s) == 0 {
 				break
 			}
-		//	fmt.Println(s)
-			val, s = s[len(s)-1], s[:len(s)-1]
-			results = append(results, val)
+
+			root, s = s[len(s)-1], s[:len(s)-1]
+			results = append(results, root.Val)
 			root = root.Right
+
 		}
-
 	}
-
 	return results
 
 }
