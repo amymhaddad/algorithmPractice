9444ee01175a2b10eac3fa4c3d9a90414769992e
Amy M Haddad <amy.m.haddad@gmail.com>
2021-09-10 07:22:30 -0400

    Solved it

--- algoExpert/go/removeDups/remove.go
+++ algoExpert/go/removeDups/remove.go
@@ -6,16 +6,17 @@ type LinkedList struct {
 	Next  *LinkedList
 }
 
-func removeDuplicatesFromLinkedList(linkedList *LinkedList) *LinkedList {
-
+//Solution 1
+func RemoveDuplicatesFromLinkedList(linkedList *LinkedList) *LinkedList {
 	curr := linkedList
 
-	for curr != nil {
+	for curr != nil && curr.Next != nil {
 		nextVal := curr.Next.Value
 		currVal := curr.Value
 
-		if nextVal == currVal {
-			curr = curr.Next.Next
+		if currVal == nextVal {
+			curr.Next = curr.Next.Next
+
 		} else {
 			curr = curr.Next
 		}
@@ -23,4 +24,19 @@ func removeDuplicatesFromLinkedList(linkedList *LinkedList) *LinkedList {
 
 	return linkedList
 
+}
+
+//Solution 2
+func RemoveDuplicatesFromLinkedList(linkedList *LinkedList) *LinkedList {
+	curr := linkedList
+
+	for curr != nil && curr.Next != nil {
+		nextVal := curr.Next
+		for nextVal != nil && nextVal.Value == curr.Value {
+			nextVal = nextVal.Next
+		}
+		curr.Next = nextVal
+		curr = curr.Next
+	}
+	return linkedList
 }
