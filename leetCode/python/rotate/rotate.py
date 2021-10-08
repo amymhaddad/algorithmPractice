2cedd453f711ba0aab4ef90b094977f55b97f734
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-08 09:53:56 -0400

    Added second solution

--- leetCode/python/rotate/rotate.py
+++ leetCode/python/rotate/rotate.py
@@ -1,4 +1,5 @@
 
+#Solution 1
 def rotate(nums, k):
 
     while k > 0:
@@ -7,3 +8,14 @@ def rotate(nums, k):
         k -= 1
     return nums
 
+#Solution 2
+def rotate(nums, k):
+    nums_len = len(nums)
+    rotated_array = [None] * nums_len
+
+    for i, num in enumerate(nums):
+        rotated_index = (k + i) % nums_len
+        rotated_array[rotated_index] = num
+    
+    for i in range(nums_len):
+        nums[i] = rotated_array[i]
