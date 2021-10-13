4fe7c7d9b0ff6a2515bbdf674c6f6bc656346b5a
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-13 08:10:10 -0400

    Solved it

--- leetCode/ll_palindrome/palindrome.py
+++ leetCode/ll_palindrome/palindrome.py
@@ -5,25 +5,49 @@
 #         self.val = val
 #         self.next = next
 
-
+#Solution 1
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         values = []
 
-        while head.next is not None:
+        while head is not None:
             values.append(head.val)
             head = head.next 
 
         p1 = 0
         p2 = len(values) -1
-
-        while p2 - p1 > 1:
+        while p1 < p2:
             if values[p2] != values[p1]:
                 return False
 
             p1 += 1
-            p2 += 1
+            p2 -= 1
         return True
 
+#Solution 2
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        values = []
+        
+        temp = head 
+        
+        while temp:
+            values.append(temp.val)
+            temp = temp.next
+  
+        while head:
+            if values.pop() != head.val:
+                return False
+            head = head.next
+        return True
 
-    
+#Solution 3
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+
+        res = []
+        
+        while head:
+            res.append(head.val)
+            head = head.next
+        return res == res[::-1]
