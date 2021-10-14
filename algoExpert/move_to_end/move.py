830306199418fb622b0ec5f1cc203a59eea0c343
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-14 10:55:18 -0400

    Sol 2 - wip

--- algoExpert/move_to_end/move.py
+++ algoExpert/move_to_end/move.py
@@ -15,7 +15,29 @@ def moveElementToEnd(array, toMove):
     return array
 
 
+
+def v2_moveElementToEnd(array, toMove):
+    p1, p2 = 0, len(array) -1
+    for i, value in enumerate(array):
+        first_num, last_num = array[p1], array[p2]
+
+        if array[p1] != array[p2]:
+            array[p1], array[p2] = array[p2], array[p1]
+            p1 += 1
+            p2 -= 1
+
+        if array[p1] != toMove:
+            p1 += 1
+
+        if array[p2] != toMove:
+            p2 -= 1
+
+    return array
+
+
+
+
 array =  [2, 1, 2, 2, 2, 3, 4, 2]
 toMove = 2
 
-print(moveElementToEnd(array, toMove))
+print(v2_moveElementToEnd(array, toMove))
