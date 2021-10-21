2ed98101b06b5f3b13690c8f8fcd7d4698e212a2
Amy M Haddad <amy.m.haddad@gmail.com>
2021-10-21 10:30:44 -0400

    First commit

---
+++ algoExpert/go/node_depths/depths.go
@@ -0,0 +1,31 @@
+package main
+
+type BinaryTree struct {
+	Value       int
+	Left, Right *BinaryTree
+}
+
+func NodeDepths(root *BinaryTree) int {
+	var totalDepth int
+
+	currLevel := []*BinaryTree{root}
+
+	var levelDepth int
+	for len(s) > 0 {
+		levelDepth = levelDepth + 1
+		nextLevel := []*BinaryTree{}
+
+		for _, node := range currLevel {
+			if node.Left != nil {
+				nextLevel = append(nextLevel, node.Left)
+			}
+			if node.Right != nil {
+				nextLevel = append(nextLevel, node.Right)
+			}
+		}
+
+		totalDepth := localDepth * len(nextLevel)
+		currLevel = nextLevel
+	}
+	return totalDepth
+}
