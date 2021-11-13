67223b9fc404c231bf4f4b3dad1c25d7fc847245
Amy M Haddad <amy.m.haddad@gmail.com>
2021-11-13 10:36:50 -0500

    Solved it

--- leetCode/python/move_zeros/move.py
+++ leetCode/python/move_zeros/move.py
@@ -1,28 +1,29 @@
 # #https://leetcode.com/problems/move-zeroes/
-#
-# def move_zeros(nums):
-#     nums.sort()
-#     first_num_index = 1
-#     for i, num in enumerate(nums):
-#         if num > 0:
-#             first_num_index = i
-#             break
-#     return nums[first_num_index:] + nums[:first_num_index]
 
+#Solution 1
+def move_zeros(nums):
+    nums.sort()
+    first_num_index = 1
+    for i, num in enumerate(nums):
+        if num > 0:
+            first_num_index = i
+            break
+    return nums[first_num_index:] + nums[:first_num_index]
 
+#Solution 2 
 def move_zeros(nums):
     p1 = 0
     p2 = 1
 
-    for num in nums:
+    for _ in range(len(nums)):
         if p2 >= len(nums):
             break 
-        if nums[p1] == 0 and nums[p2] > 0:
+        if nums[p1] != 0:
+            p1 += 1
+            p2 += 1 
+        elif nums[p1] == 0 and nums[p2] != 0:
             nums[p1], nums[p2] = nums[p2], nums[p1]
             p1 += 1
             p2 += 1
         elif nums[p1] == 0 and nums[p2] == 0:
             p2 += 1
-    return nums
-
-print(move_zeros([1,0,1]))
--- leetCode/python/move_zeros/test_move.py
+++ leetCode/python/move_zeros/test_move.py
@@ -7,3 +7,4 @@ def test_move_zeros():
     assert move_zeros([0,1,0,3,12]) == [1,3,12,0,0]
     assert move_zeros([0, 0]) == [0, 0]
     assert move_zeros([0, 1]) == [1, 0]
+    assert move_zeros([1, 0, 1]) == [1, 1, 0] 
