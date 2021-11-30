28fe63fcc00ad07570cf05fc76aaa91cbefd7d8d
Amy M Haddad <amy.m.haddad@gmail.com>
2021-11-30 10:59:54 -0500

    Added second solution

--- leetCode/python/palindrome_number/palindrome.py
+++ leetCode/python/palindrome_number/palindrome.py
@@ -2,22 +2,44 @@
 https://leetcode.com/problems/palindrome-number/
 """
 
-def isPalindrome(x):
-    if x < 0:
-        return False
+# def isPalindrome(x):
+#     if x < 0:
+#         return False
+#
+#     if x > 0 and x < 10:
+#         return True
+#
+#     num_string = str(x)
+#     p1 = 0
+#     p2 = len(num_string) -1 
+#     
+#     while p2 - p1 >= 1:
+#         if num_string[p1] != num_string[p2]:
+#             return False
+#         else:
+#             p1 += 1
+#             p2 -= 1
+#     return True
 
-    if x < 10 and x > 0:
-        return True
+# def isPalindrome(x):
 
-    num_string = str(x)
-    p1 = 0
-    p2 = len(num_string) -1 
-    
-    while p2 - p1 >= 1:
-        if num_string[p1] != num_string[p2]:
-            return False
-        else:
-            p1 += 1
-            p2 -= 1
-    return True
+#     if x < 0:
+#         return False
+
+#     if x > 0 and x < 10:
+#         return True
+
+#     original_num = x 
+#     new_num = []
+
+#     while original_num > 0:
+#         new_num.append(original_num % 10)
+#         original_num = original_num // 10
+
+#     for num in new_num[::-1]:
+#         if num != x % 10:
+#             return False
+#         x = x // 10
+
+#     return True
 
