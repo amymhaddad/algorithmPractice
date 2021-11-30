9e77184d876f2b955ed21c39fbd036f75e9b00e0
Amy M Haddad <amy.m.haddad@gmail.com>
2021-11-30 10:39:03 -0500

    Added first solution

--- leetCode/python/palindrome_number/test.py
+++ leetCode/python/palindrome_number/test.py
@@ -6,5 +6,7 @@ def test_palindrome():
     assert isPalindrome(121) == True
     assert isPalindrome(123) == False
     assert isPalindrome(10) == False
+    assert isPalindrome(-101) == False
+    assert isPalindrome(000) == True
 
 
