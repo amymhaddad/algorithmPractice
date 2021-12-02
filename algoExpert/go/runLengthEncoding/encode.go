1cdce86db769e136ac1d06a8eb1b93d1de4450bb
Amy M Haddad <amy.m.haddad@gmail.com>
2021-12-02 10:47:37 -0500

    Added first implementation of algorithm

--- algoExpert/go/runLengthEncoding/encode.go
+++ algoExpert/go/runLengthEncoding/encode.go
@@ -22,14 +22,24 @@ func RunLengthEncoding(str string) string {
 		}
 
 		currLetter = strings.ToUpper(string(ch))
-		if (len(letters) == 0) || (len(letters) <= 9 && currLetter == letters[len(letters)-1]) {
+
+		//If I've found the same letter but the length of letters is >= 9, then I need to create a new slice
+		if len(letters) >= 9 && currLetter == letters[len(letters)-1] {
+			encode = append(encode, strconv.Itoa(count)+letters[len(letters)-1])
+			letters = []string{currLetter}
+			count = 1
+			continue
+		}
+
+		//IF I'm at the start of the string OR I have a repeating letter and the length of the string is less than or == to 9
+		if (len(letters) == 0) || len(letters) <= 9 && currLetter == letters[len(letters)-1] {
 			letters = append(letters, currLetter)
 			count++
 			continue
 		}
 
+		//I've found a NEW letter
 		if currLetter != letters[len(letters)-1] {
-			//countAsString := string(count)
 			encode = append(encode, strconv.Itoa(count)+letters[len(letters)-1])
 			if count > 0 {
 				count = 0
@@ -37,39 +47,17 @@ func RunLengthEncoding(str string) string {
 			}
 			letters = append(letters, currLetter)
 			count++
-			fmt.Println("c", encode)
 			continue
 		}
 
-		// 	fmt.Println(count, letters)
-		// } else if (len(letters) == 0) || (len(letters) < 10 && currLetter == letters[len(letters)-1]) {
-		// 	letters = append(letters, currLetter)
-		// 	count++
-		// }
-
-		// if len(letters) >= 9 && currLetter == letters[len(letters)-1] {
-		// 	encode = append(encode, string(count)+letters[len(letters)-1])
-		// 	letters = []string{currLetter}
-		// 	count = 1
-		//}
-		// if len(letters) > 1 && currLetter != letters[len(letters)-1] {
-		// 	encode = append(encode, string(count)+letters[len(letters)-1])
-		// 	letters = []string{currLetter}
-		// 	count++
-
-		// 	fmt.Println(count, letters)
-		// } else if (len(letters) == 0) || (len(letters) < 10 && currLetter == letters[len(letters)-1]) {
-		// 	letters = append(letters, currLetter)
-		// 	count++
-		// }
-
 	}
-	encode = append(encode, string(count)+letters[len(letters)-1])
+	encode = append(encode, strconv.Itoa(count)+letters[len(letters)-1])
 	return strings.Join(encode, "")
 
 }
 
 func main() {
-	x := RunLengthEncoding("1AAB")
+	x := RunLengthEncoding("AAAAAAAAAAAAABBCCCCDD")
 	fmt.Println(x)
+	//10A3A2B4C2D
 }
